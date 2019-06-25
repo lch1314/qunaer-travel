4ee0217416715c91d9a7a84fb3598801936400ab
liuchenghu <786973980@qq.com>
2019-06-25 10:35:02 +0800

    优化代码

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -3,8 +3,9 @@
         <div class="header-left">
             <div class="iconfont back-icon">&#xe682;</div>
         </div>
-        <div class="header-input">
-            <span class="iconfont">&#xe613;</span>输入城市/景点/游玩主题
+        <div class="header-center">
+            <span class="iconfont search-icon">&#xe613;</span>
+            <input class="header-input" placeholder="输入城市/景点/游玩主题" />
         </div>
         <div class="header-right">城市<span class="iconfont arrow-icon">&#xe64a;</span></div>
     </div>
@@ -26,22 +27,38 @@ export default {
         line-height: .86rem
         background: $bgColor
         color: #fff
+        position: relative
         .header-left
             width: .64rem
             float: left
             .back-icon
                 text-align: center
                 font-size: .4rem
-        .header-input
+        .header-center
             flex: 1
             height: .64rem
             line-height: .64rem
             margin-top: .12rem
             margin-left: .2rem
-            padding-left: .2rem
+            padding: 0 .4rem
             background: #fff
             border-radius: .1rem
             color: #ccc
+            .header-input
+                width: 100%
+                border-radius: .1rem
+            input::-webkit-input-placeholder
+                color: #ccc
+            input::-moz-placeholder
+                color: #ccc
+            input:-moz-placeholder
+                color: #ccc
+            input:-ms-input-placeholder
+                color: #ccc
+            .search-icon
+                position: absolute
+                top: .15rem
+                left: .94rem
         .header-right
             width: 1.24rem
             float: right
