dac47ab3f8eee9b8c728c1999cf64af7b7bada79
liuchenghu <786973980@qq.com>
2019-06-27 09:51:04 +0800

    实现Header渐隐渐显效果

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <div class="iconfont header-abs-back">&#xe682;</div>
+    </router-link>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      景点详情
+      <router-link to="/">
+        <div class="iconfont header-fixed-back">&#xe682;</div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // 组件名字
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 20) {
+        let opacity = top / 100
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header-abs
+    position: absolute
+    left: .2rem
+    top: .2rem
+    width: .8rem
+    height: .8rem
+    border-radius: .4rem
+    background: rgba(0,0,0,.8)
+    text-align: center
+    line-height: .8rem
+    .header-abs-back
+      color: #ffffff
+      font-size: .4rem
+  .header-fixed
+    height: $headerHeight
+    line-height: $headerHeight
+    color: #fff
+    text-align: center
+    background: $bgColor
+    font-size: .32rem
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    .header-fixed-back
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      position: absolute
+      top: 0
+      left: .2rem
+      color: #fff
+</style>
